1996076c80333994f9f9dfda875e3bd2a6238a1d
Anthony Louis <anthony@simbioseventures.com>
2020-08-26 10:22:02 -0300

    Add builder for Country

--- api-jax-rs-test/src/main/java/me/anthonylouis/server/entity/Country.java
+++ api-jax-rs-test/src/main/java/me/anthonylouis/server/entity/Country.java
@@ -2,6 +2,16 @@ package me.anthonylouis.server.entity;
 
 public class Country {
 
+  public Country(final int id, final String name, final double maxLatitude,
+      final double minLatitude, final double maxLongitude, final double minLongitude) {
+    this.id = id;
+    this.name = name;
+    this.maxLatitude = maxLatitude;
+    this.minLatitude = minLatitude;
+    this.maxLongitude = maxLongitude;
+    this.minLongitude = minLongitude;
+  }
+
   private int id;
 
   private String name;
@@ -60,5 +70,56 @@ public class Country {
 
   public void setMinLongitude(final double minLongitude) {
     this.minLongitude = minLongitude;
+  }
+
+  public static class Builder {
+
+    private int id = 0;
+    private String name = "";
+    private double maxLatitude = 0.0;
+    private double minLatitude = 0.0;
+    private double maxLongitude = 0.0;
+    private double minLongitude = 0.0;
+
+    public Builder withId(final int id) {
+      this.id = id;
+
+      return this;
+    }
+
+    public Builder withName(final String name) {
+      this.name = name;
+
+      return this;
+    }
+
+    public Builder withMaxLatitude(final double maxLatitude) {
+      this.maxLatitude = maxLatitude;
+
+      return this;
+    }
+
+    public Builder withMinLatitude(final double minLatitude) {
+      this.minLatitude = minLatitude;
+
+      return this;
+    }
+
+    public Builder withMaxLongitude(final double maxLongitude) {
+      this.maxLongitude = maxLongitude;
+
+      return this;
+    }
+
+    public Builder withMinLongitude(final double minLongitude) {
+      this.minLongitude = minLongitude;
+
+      return this;
+    }
+
+    public Country build() {
+      return new Country(this.id, this.name, this.maxLatitude, this.minLatitude, this.maxLongitude,
+          this.minLongitude);
+    }
   }
 }

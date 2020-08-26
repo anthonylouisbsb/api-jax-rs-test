eb00ebacc799b72b414c8a17959fa5a7448b68fa
Anthony Louis <anthony@simbioseventures.com>
2020-08-26 10:44:23 -0300

    Add class to handle JSON operations

---
+++ api-jax-rs-test/src/main/java/me/anthonylouis/utils/json/JsonUtils.java
@@ -0,0 +1,26 @@
+package me.anthonylouis.utils.json;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/**
+ * Is an wrapper for Jackson library, used to read/write JSON
+ */
+public enum JsonUtils {
+  INSTANCE;
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+
+  /**
+   * Serialize an object to a JSON string
+   *
+   * @param objectToBeSerialized a generic object to be serialized
+   * @return the object in JSON format
+   * @throws JsonProcessingException if Jackson can not serialize the object
+   */
+  public String writeObjectAsJsonString(final Object objectToBeSerialized)
+      throws JsonProcessingException {
+    return objectMapper.writeValueAsString(objectToBeSerialized);
+  }
+}

88bbab370b370d695864d52960570fcd6db78680
Anthony Louis <anthony@simbioseventures.com>
2020-09-03 11:45:20 -0300

    Add enum about dao implementation

---
+++ api-jax-rs-test/src/main/java/me/anthonylouis/server/dao/DaoImplementation.java
@@ -0,0 +1,28 @@
+package me.anthonylouis.server.dao;
+
+/**
+ * Represents the many data sources that can be used by the program
+ */
+public enum DaoImplementation {
+  MONGODB("me.anthonylouis.server.dao.CountryMongoDbDao");
+
+  private final String countryDaoImplementationClass;
+
+  DaoImplementation(final String countryDaoImplementationClass) {
+    this.countryDaoImplementationClass = countryDaoImplementationClass;
+  }
+
+  public String getCountryDaoImplementationClass() {
+    return this.countryDaoImplementationClass;
+  }
+
+  /**
+   * Returns the type of the DAO that must be used by the application
+   *
+   * @param usedDatabase the database used by application(mongods, postgres..)
+   * @return the object with which DAO instance must be used by the program
+   */
+  public static DaoImplementation fromString(final String usedDatabase) {
+    return MONGODB;
+  }
+}

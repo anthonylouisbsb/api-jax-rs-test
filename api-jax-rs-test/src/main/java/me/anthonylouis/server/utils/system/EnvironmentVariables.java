8b29f2e8a230121540bc45e4bf4d2e4efabbb5b6
Anthony Louis <anthony@simbioseventures.com>
2020-09-03 12:28:24 -0300

    Add class used to handle environment variables

---
+++ api-jax-rs-test/src/main/java/me/anthonylouis/server/utils/system/EnvironmentVariables.java
@@ -0,0 +1,13 @@
+package me.anthonylouis.server.utils.system;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * Responsible to retrieve all program necessary environment variables
+ */
+public abstract class EnvironmentVariables {
+
+  public static String getEnvironmentVariable(@NotNull final String environmentVariable) {
+    return System.getenv(environmentVariable);
+  }
+}
